d77df1cf565f1f3ed90f54d8961593461e0197bd
Chenguang Liu <chenguang.liu@appfolio.com>
2017-07-14 14:58:21 -0700

    Add column online in user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714210030) do
+ActiveRecord::Schema.define(version: 20170714215615) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "online", default: false
   end
 
 end
