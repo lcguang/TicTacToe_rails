cd34ad62e99deedfac13aa78161bf791699eb7c2
Chenguang Liu <chenguang.liu@appfolio.com>
2017-07-14 14:21:36 -0700

    Add new create show and destory actions in users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tic_tac_toe#index'
   resources :tic_tac_toe, only: [:index]
+  resources :users
 end
