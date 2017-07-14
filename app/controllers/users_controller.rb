3bd7f291b31696d0240e53dcc7fae9b957c70630
Chenguang Liu <chenguang.liu@appfolio.com>
2017-07-14 15:30:51 -0700

    Add index action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -7,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = 'Successfully created!'
-      redirect_to root_url
+      redirect_to @user
     else
       render 'new'
     end
